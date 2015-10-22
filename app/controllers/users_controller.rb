6036daaaaea12a8a85036c5ac290ed14c14818bc
Daniel <dgrenier@smallbizpros.com>
2015-10-22 19:36:40 +0000

    Added file fields for image upload and display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :autnthicate_user!
+    before_action :authenticate_user!
     def show
         @user = User.find(params[:id])
     end
